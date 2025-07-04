a87204823a63547c1d75a94194ef03af6f2f3d50
Subhani55 <m.mohsinalisubhani5@gmail.com>
2025-07-04 17:05:34 +0500

    fix bug

--- api/index.js
+++ api/index.js
@@ -37,8 +37,8 @@ app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
 // Create to handle errors
